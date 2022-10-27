1d85e7eb448c3c47b52a9cf5e3104d5b370cd4b0
MyGuy86 <milesmorel86@gmail.com>
2022-10-27 15:56:51 -0400

    deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,8 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 set :rbenv_roles, :all # default value
 
-set :application, "Miles Morel"
-set :repo_url, "https://github.com/MyGuy86/RoR_Rocket_Elevators_Information_System.git"
+set :application, "MilesMorel"
+set :repo_url, "git@github.com:MyGuy86/RoR_Rocket_Elevators_Information_System.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
