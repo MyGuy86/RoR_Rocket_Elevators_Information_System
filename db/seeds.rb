3c986d3434a2b5e1d62d3442573c8e1f0becb97a
Perk318 <mp3_2004@icloud.com>
2022-11-02 14:03:02 -0400

    seeded quotes 1/2

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,18 +26,24 @@ require 'csv'
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-# require 'csv'
-# csvfile = File.read(Rails.root.join('lib', 'seeds', 'EmployeeList.csv'))
-# # CSV.read("EmployeeList.csv")
-# table = CSV.parse(csvfile, headers: true)
-# table.each do |row|
-#    EmployeeList.create!(
-#         first_name: Faker::FunnyName.name,
-#         last_name: row['last_name'],
-#         title: row['title'],
-#         email: row['email'],
+require 'csv'
+csvfile = File.read(Rails.root.join('lib', 'seeds', 'EmployeeList.csv'))
+# CSV.read("EmployeeList.csv")
+table = CSV.parse(csvfile, headers: true)
+table.each do |row|
+   EmployeeList.create!(
+        first_name: Faker::FunnyName.name,
+        last_name: row['last_name'],
+        title: row['title'],
+        email: row['email'],
 #     )
-# end
+# # end
+
+
+
+
+
+
 
 require 'faker'
 100.times do 
@@ -55,20 +61,119 @@ require 'faker'
 end 
 # puts "*(*******************seededlead:db ********************* "
 
-Quote.create!(
-	building_type: ['residential', 'corporate', 'hybrid', 'commercial'].sample
-    service_quality:['standard', 'premium', 'excelium'].sample 
-    number_of_apartments: "number_of_apartments"
-    number_of_floors: "number_of_floors"
-    number_of_businesses: "number_of_businesses"
-    number_of_basements: "number_of_basements"
-    number_of_parking: "number_of_parking"
-    number_of_cages: "number_of_cages"
-    number_of_occupants: "number_of_occupants"
-    number_of_hours:Faker::Number.within(range: 1..24)
-    number_of_elevators_needed:
-    price_per_unit:
-    elevator_price:
-    installation_fee:
-    final_price:
-)
+
+
+
+# # Quote.all.each do |q|
+# 	if building_type == "residential"
+# 		number_of_apartments1 = Faker::Number.between(from: 40, to: 350)
+# 		number_of_floors1 = Faker::Number.number(digits: 2)
+# 		number_of_businesses1 = 'null'
+# 		number_of_basements1 = 'null'
+# 		number_of_parking1 = 'null'
+# 		number_of_cages1 = 'null'
+# 		number_of_occupants1 = 'null'
+# 		number_of_hours1 = 'null'
+	
+# 	end
+	
+	# building_type: ["residential", "corporate", "hybrid", "commercial"].sample,
+	# service_quality:["standard", "premium", "excelium"].sample, 
+   
+	require 'faker'
+	# 50.times do
+	# 	Quote.create!(
+		# building_type: ["residential", "corporate", "hybrid", "commercial"].sample,
+		# service_quality:["standard", "premium", "excelium"].sample, 	
+		# if building_type == "residential" 
+		# 	number_of_apartments1 = Faker::Number.between(from: 40, to: 350)
+		# 	number_of_floors1 = Faker::Number.number(digits: 2)
+		# 	number_of_businesses1 = 'null'
+		# 	number_of_basements1 = Faker::Number.number(digits: 2)
+		# 	number_of_parking1 = 'null'
+		# 	number_of_cages1 = 'null'
+		# 	number_of_occupants1 = 'null'
+		# 	number_of_hours1 = 'null'
+		# 	number_of_elevators_needed= Faker::Number.number(digits: 2)
+    	# 	price_per_unit= Faker::Number.decimal(l_digits: 2)
+   		# 	elevator_price= Faker::Number.decimal(l_digits: 3, r_digits: 2)
+    	# 	installation_fee= Faker::Number.decimal(l_digits: 3, r_digits: 2)
+   		# 	final_price= Faker::Number.decimal(l_digits: 4, r_digits: 2)
+		# end
+		
+
+	
+		# if building_type == "corporate" 
+		# 	number_of_apartments= 'null'
+		# 	number_of_floors= Faker::Number.number(digits: 2),
+		# 	number_of_businesses= Faker::Number.number(digits: 3),
+		# 	number_of_basements= Faker::Number.non_zero_digit ,
+		# 	number_of_parking= Faker::Number.number(digits: 3),
+		# 	number_of_cages= 'null',
+		# 	number_of_occupants= 'null',
+		# 	number_of_hours= 'null',
+		# 	number_of_elevators_needed= 'null'
+		# 	price_per_unit= Faker::Number.decimal(l_digits: 2) 
+		# 	elevator_price= Faker::Number.decimal(l_digits: 3, r_digits: 2)
+		# 	installation_fee= Faker::Number.decimal(l_digits: 3, r_digits: 2)
+		# 	final_price= Faker::Number.decimal(l_digits: 4, r_digits: 2)
+		# end 
+
+		# if building_type == "commercial"
+		# 	number_of_apartments= 'null'
+		# 	number_of_floors= Faker::Number.number(digits: 2),
+		# 	number_of_businesses= Faker::Number.number(digits: 3),
+		# 	number_of_basements= Faker::Number.non_zero_digit ,
+		# 	number_of_parking= Faker::Number.number(digits: 3),
+		# 	number_of_cages= 'null',
+		# 	number_of_occupants= 'null',
+		# 	number_of_hours= 'null',
+		# 	number_of_elevators_needed= 'null'
+		# 	price_per_unit= Faker::Number.decimal(l_digits: 2) 
+		# 	elevator_price= Faker::Number.decimal(l_digits: 3, r_digits: 2)
+		# 	installation_fee= Faker::Number.decimal(l_digits: 3, r_digits: 2)
+		# 	final_price= Faker::Number.decimal(l_digits: 4, r_digits: 2)
+		# end 
+
+		# if building_type == "hybrid"
+		# 	number_of_apartments= 'null'
+		# 	number_of_floors= Faker::Number.number(digits: 2)
+		# 	number_of_businesses= Faker::Number.number(digits: 3)
+		# 	number_of_basements= Faker::Number.non_zero_digit 
+		# 	number_of_parking= Faker::Number.number(digits: 3)
+		# 	number_of_cages= 'null'
+		# 	number_of_occupants= Faker::Number.number(digits: 3)
+		# 	number_of_hours= Faker::Number.between(from: 1, to: 24)
+		# 	number_of_elevators_needed= Faker::Number.number(digits: 2)
+		# 	price_per_unit= Faker::Number.decimal(l_digits: 2) 
+		# 	elevator_price= Faker::Number.decimal(l_digits: 3, r_digits: 2)
+		# 	installation_fee= Faker::Number.decimal(l_digits: 3, r_digits: 2)
+		# 	final_price= Faker::Number.decimal(l_digits: 4, r_digits: 2)
+
+
+
+	
+	50.times do
+		Quote.create!(
+			building_type: ["residential", "corporate", "hybrid", "commercial"].sample,
+			service_quality:["standard", "premium", "excelium"].sample, 
+			number_of_apartments: Faker::Number.between(from: 40, to: 350),
+			number_of_floors: Faker::Number.number(digits: 2),
+			number_of_businesses: Faker::Number.number(digits: 2),
+			number_of_basements: Faker::Number.non_zero_digit, 
+			number_of_parking: Faker::Number.number(digits: 3),
+			number_of_cages: Faker::Number.number(digits: 2),
+			number_of_occupants: Faker::Number.number(digits: 4),
+			number_of_hours: Faker::Number.within(range: 1..24),
+			number_of_elevators_needed: Faker::Number.number(digits: 2),
+			price_per_unit: Faker::Number.decimal(l_digits: 2), 
+			elevator_price: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+			installation_fee: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+			final_price: Faker::Number.decimal(l_digits: 4, r_digits: 2),
+
+
+		)
+	end
+
+
+puts "*(*******************seededquote:db ********************* "
